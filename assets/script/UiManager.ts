369ac814d19b786e1c82e296774b46b6c8240bda
Link <timelyeee@gmail.com>
2021-07-11 02:00:48 +0800

    add cargos

--- assets/script/UiManager.ts
+++ assets/script/UiManager.ts
@@ -1,6 +1,7 @@
 
-import { _decorator, Component, Node,Animation, Camera, Button, Canvas, Layout, EventHandler, Font} from 'cc';
+import { _decorator, Component, Node,Animation, Camera, Button, Canvas, Layout, EventHandler, Font, BlockInputEvents} from 'cc';
 import { LevelGenerater } from './level-generater';
+import { Reposition } from './reposition';
 const { ccclass, property } = _decorator;
 
 @ccclass('UiManager')
@@ -15,6 +16,8 @@ export class UiManager extends Component {
     private leftAnim :Animation|null = null;
     private rightAnim :Animation|null = null;
 
+    public currentFunctionCall: Function = this.changeLevel1;
+
     @property(Button)
     public levelButtons:Button[]=[];
 
@@ -24,78 +27,133 @@ export class UiManager extends Component {
     @property(Button)
     public menuButton: Button|null = null;
 
-    public isUi:boolean = true;
+    public isInScene:boolean = false;
 
     @property(Canvas)
     public levelCanvas:Canvas|null = null;
 
+    @property(Node)
+    public BlockNode:Node|null = null;
+
     public levelGenerater :LevelGenerater|null=null;
     start () {
         //const leftAnim =  this.leftNodeToMove!.getComponent(Animation);
-        this.isUi = true;
+        this.isInScene = false;
         this.rightAnim =  this.rightNodeToMove!.getComponent(Animation);
         this.leftAnim =  this.leftNodeToMove!.getComponent(Animation);
         this.levelGenerater = this.levelCanvas!.getComponent(LevelGenerater);
+        this.rightAnim =  this.rightNodeToMove!.getComponent(Animation);
+        this.leftAnim =  this.leftNodeToMove!.getComponent(Animation);
+        for(var b of this.levelButtons){
+            b.addComponent(Reposition);
+        }
+        for(var v of this.leftNodeToMove!.children)
+        {
+            v.addComponent(Reposition);
+        }
+        for(var v of this.rightNodeToMove!.children)
+        {
+            v.addComponent(Reposition);
+        }
+
+
     }
     openScene(){
         
-        this.rightAnim =  this.rightNodeToMove!.getComponent(Animation);
-        this.leftAnim =  this.leftNodeToMove!.getComponent(Animation);
-        console.log("open scene");
         this.leftAnim!.play();
         this.rightAnim!.play();
         this.menuButton!.node.active = true;
+        this.isInScene=true;
+        this.BlockNode!.active=false;
     }
     OpenMenu(){
         this.menuLayout!.node.active = true;
-
+        this.BlockNode!.active=true;
     }
     continue(){
         this.menuLayout!.node.active = false;
+        this.BlockNode!.active=false;
     }
     goHome(){
-        this.menuButton!.node.active = false;
+        this.rightAnim!.getState("RightMove").speed = -1;
+        this.rightAnim!.play();
+        this.rightAnim!.once(Animation.EventType.FINISHED,()=>{
+            this.rightAnim!.getState("RightMove").speed = 1;
+        })
+        this.leftAnim!.getState("MoveToLeft").speed = -1;
+        this.leftAnim!.play();
+        this.leftAnim!.once(Animation.EventType.FINISHED,()=>{
+            this.leftAnim!.getState("MoveToLeft").speed = 1;
+        })
+        for(var b of this.levelButtons){
+            b.getComponent(Reposition)?.resetPosition();
+        }
+        this.menuLayout!.node.active=false;
+        this.BlockNode!.active=true;
     }   
     
     //饶了，不然有bug
     changeLevel1( ){
+        // if(this.currentFunctionCall !=this.changeLevel1 )
+        //     this.currentFunctionCall = this.changeLevel1;
         this.levelGenerater?.clear();
         this.levelGenerater!.loadScene(0);
-        this.openScene();
+        
+        if(!this.isInScene){
         const buttonAnimation = this.levelButtons[0].getComponent(Animation);
         buttonAnimation?.play("1Lv1");
         this.levelButtons[1].getComponent(Animation)?.play("2Lvn");
         this.levelButtons[2].getComponent(Animation)?.play("3Lvn");
-        this.levelButtons[3].getComponent(Animation)?.play("4Lvn");
+        this.levelButtons[3].getComponent(Animation)?.play("4Lvn");}
+        this.openScene();
     }
     changeLevel2( ){
+        if(this.currentFunctionCall !=this.changeLevel2 )
+            this.currentFunctionCall = this.changeLevel2;
         this.levelGenerater?.clear();
         this.levelGenerater!.loadScene(1);
-        this.openScene();
+        
+        if(!this.isInScene){
         const buttonAnimation = this.levelButtons[1].getComponent(Animation);
         buttonAnimation?.play("2Lv2");
         this.levelButtons[0].getComponent(Animation)?.play("1Lvn");
         this.levelButtons[2].getComponent(Animation)?.play("3Lvn");
-        this.levelButtons[3].getComponent(Animation)?.play("4Lvn");
+        this.levelButtons[3].getComponent(Animation)?.play("4Lvn");}
+        this.openScene();
     }changeLevel3( ){
+        if(this.currentFunctionCall !=this.changeLevel3 )
+            this.currentFunctionCall = this.changeLevel3;
         this.levelGenerater?.clear();
         this.levelGenerater!.loadScene(2);
-        this.openScene();
+        
+        if(!this.isInScene){
         const buttonAnimation = this.levelButtons[2].getComponent(Animation);
         buttonAnimation?.play("3Lv3");
         this.levelButtons[1].getComponent(Animation)?.play("2Lvn");
         this.levelButtons[0].getComponent(Animation)?.play("1Lvn");
-        this.levelButtons[3].getComponent(Animation)?.play("4Lvn");
+        this.levelButtons[3].getComponent(Animation)?.play("4Lvn");}
+        this.openScene();
     }changeLevel4( ){
+        if(this.currentFunctionCall !=this.changeLevel4 )
+            this.currentFunctionCall = this.changeLevel4;
         this.levelGenerater?.clear();
         this.levelGenerater!.loadScene(3);
-        this.openScene();
+        
+        if(!this.isInScene){
         const buttonAnimation = this.levelButtons[3].getComponent(Animation);
         buttonAnimation?.play("4Lv4");
         this.levelButtons[1].getComponent(Animation)?.play("2Lvn");
         this.levelButtons[2].getComponent(Animation)?.play("3Lvn");
-        this.levelButtons[0].getComponent(Animation)?.play("1Lvn");
+        this.levelButtons[0].getComponent(Animation)?.play("1Lvn");}
+        this.openScene();
     }
+
+    restart(){
+        this.currentFunctionCall();
+        this.continue();
+    }
+    
+
 }
 
 /**
---
+++ assets/script/reposition.ts
@@ -0,0 +1,31 @@
+
+import { _decorator, Component, Node, Vec3 } from 'cc';
+const { ccclass, property } = _decorator;
+
+@ccclass('Reposition')
+export class Reposition extends Component {
+    // [1]
+    // dummy = '';
+
+    // [2]
+    // @property
+    // serializableDummy = 0;
+    public originPosition: Vec3 = new Vec3;
+    start () {
+        this.originPosition = this.node.position;
+    }
+    public resetPosition(){
+        this.node.setPosition(this.originPosition);
+    }
+}
+
+/**
+ * [1] Class member could be defined like this.
+ * [2] Use `property` decorator if your want the member to be serializable.
+ * [3] Your initialization goes here.
+ * [4] Your update function goes here.
+ *
+ * Learn more about scripting: https://docs.cocos.com/creator/3.0/manual/en/scripting/
+ * Learn more about CCClass: https://docs.cocos.com/creator/3.0/manual/en/scripting/ccclass.html
+ * Learn more about life-cycle callbacks: https://docs.cocos.com/creator/3.0/manual/en/scripting/life-cycle-callbacks.html
+ */
