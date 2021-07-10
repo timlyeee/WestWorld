a53079c750e7bbf2f467d473cbe19c1400a2a390
zhangxin <59186205+chichinohaha@users.noreply.github.com>
2021-07-10 15:37:13 +0800

    添加了 Cargo ChargeNode SwitchNode TrackNode Train
    Cargo 货物
    ChargeNode 交换站
     SwitchNode  可切换的轨道
    TrackNode  轨道基类
    Train 火车

--- assets/script/Train.ts
+++ assets/script/Train.ts
@@ -1,23 +1,61 @@
 
-import { _decorator, Component, Node } from 'cc';
-const { ccclass, property } = _decorator;
-
+import { _decorator, Component, Node, SpriteComponent, SpriteFrame } from 'cc';
+import { Cargo } from './Cargo';
+import { TrackNode } from './TrackNode';
+const { ccclass, property , type ,requireComponent} = _decorator;
 @ccclass('Train')
+@requireComponent(SpriteComponent)
 export class Train extends Component {
-    // [1]
-    // dummy = '';
-
-    // [2]
-    // @property
-    // serializableDummy = 0;
-
-    start () {
-        // [3]
+    move(){
+        this.currentNode?.responeTrain(this);
+        if(this.backwardTrain){
+            this.backwardTrain.move();
+        }
+    }
+    get forwardTrain(){
+        return this.forwardNode?.getComponent(Train)
+    };
+    get backwardTrain(){
+        return this.backwardNode?.getComponent(Train)
+    }
+    @type(Node)
+    forwardNode:Node|null = null;
+    @type(Node)
+    backwardNode:Node|null = null;
+    @type(TrackNode)
+    _currentNode:TrackNode|null =null;
+    @type(TrackNode)
+    get currentNode(){
+        return this._currentNode;
+    };
+    set currentNode(value){
+        if(value){
+            this._currentNode = value;
+            this.node.position = value.node.position
+        }
+    }
+    @type([SpriteFrame])
+    spriteFrameArray:SpriteFrame[] =[];
+    @type(TrackNode)
+    lastNode:TrackNode|null=null;
+    @type(Cargo)
+    _cargo:Cargo|null=null;
+    @type(Cargo)
+    get cargo(){
+        return this._cargo;
+    }
+    set cargo(value){
+        if(value){
+            const spriteFrame = this.spriteFrameArray.find((frame,index)=>{ index +1 === value.type});
+            if(spriteFrame){
+                this.getComponent(SpriteComponent)!.spriteFrame = spriteFrame;
+            }
+        } else {
+            const spriteFrame = this.spriteFrameArray[0];
+            this.getComponent(SpriteComponent)!.spriteFrame = spriteFrame;
+        }
     }
 
-    // update (deltaTime: number) {
-    //     // [4]
-    // }
 }
 
 /**
