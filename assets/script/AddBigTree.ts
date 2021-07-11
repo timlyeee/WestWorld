5e9f21186b28aea7afaedbb348b9fe4cc82501a9
Link <timelyeee@gmail.com>
2021-07-11 13:54:02 +0800

    nope

---
+++ assets/script/AddBigTree.ts
@@ -0,0 +1,34 @@
+
+import { _decorator, Component, Node, Prefab, instantiate, Vec3 } from 'cc';
+const { ccclass, property } = _decorator;
+
+@ccclass('AddBigTree')
+export class AddBigTree extends Component {
+    // [1]
+    // dummy = '';
+
+    // [2]
+    // @property
+    // serializableDummy = 0;
+    @property(Prefab)
+    public bigTree:Prefab | null = null;
+    start () {
+        const bigTree = instantiate(this.bigTree!);
+        this.node.addChild(bigTree);
+        bigTree.position = new Vec3(-170.304, 275.173,0);
+        
+        
+    }
+
+}
+
+/**
+ * [1] Class member could be defined like this.
+ * [2] Use `property` decorator if your want the member to be serializable.
+ * [3] Your initialization goes here.
+ * [4] Your update function goes here.
+ *
+ * Learn more about scripting: https://docs.cocos.com/creator/3.0/manual/en/scripting/
+ * Learn more about CCClass: https://docs.cocos.com/creator/3.0/manual/en/scripting/ccclass.html
+ * Learn more about life-cycle callbacks: https://docs.cocos.com/creator/3.0/manual/en/scripting/life-cycle-callbacks.html
+ */
